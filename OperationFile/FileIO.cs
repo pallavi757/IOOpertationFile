ec03b8c378db7e19d88509cce3c199bf22f5e956
unknown <kinekarpallavi21@gmail.com>
2022-04-15 11:39:47 +0530

    Perform differnt opertion on file

---
+++ OperationFile/FileIO.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OperationFile
+{
+    public class Files
+    {
+        public string filePath = @"H:\VS\OperationFile\text1.txt";
+        public void FileReadAllText()
+        {
+
+            string testData = File.ReadAllText(filePath);
+            Console.WriteLine(testData);
+        }
+        public void FileReadAllLines()
+        {
+            string[] testDataLineByLine = File.ReadAllLines(filePath);
+            foreach (var item in testDataLineByLine)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void FileReadAllBytes()
+        {
+            byte[] testDataRawBytes = File.ReadAllBytes(filePath);
+            foreach (var item in testDataRawBytes)
+            {
+                Console.Write(item);
+            }
+        }
+
+        public void FileWriteAllText()
+        {
+            string data = "C# Corner MVP & Microsoft MVP;";
+            File.WriteAllText(filePath, data);
+        }
+        public void FileWriteAllLines()
+        {
+            string[] data = { "MCT", "MCPD", "MCTS", "MCSD.NET", "MCAD.NET", "CSM" };
+            File.WriteAllLines(filePath, data);
+        }
+        public void FileAppendAllText()
+        {
+            string data = "Also Certified from IIT Kharagpur";
+            File.AppendAllText(filePath, data);
+        }
+        public void FileAppendAllLines()
+        {
+            string[] otherData = { "Worked with Microsoft", "Lived in USA" };
+            File.AppendAllLines(filePath, otherData);
+        }
+        public void FileCopy()
+        {
+            string destinationFilePath = @"H:\VS\OperationFile\Text2.txt";
+            bool overWrite = true;
+            File.Copy(filePath, destinationFilePath, overWrite);
+            Console.WriteLine("File Copy Successfully");
+        }
+        public void FileDelete()
+        {
+            string destinationFilePath = @"H:\VS\OperationFile\Text2.txt";
+            File.Delete(destinationFilePath);
+        }
+        public void FileExistorNot()
+        {
+            bool doesFileExist = File.Exists(filePath);
+            Console.WriteLine(doesFileExist);
+        }
+        public void GetFileCreationTime()
+        {
+            DateTime fileCreatedOn = File.GetCreationTime(filePath);
+            Console.WriteLine(fileCreatedOn);
+        }
+        public void FileInfo()
+        {
+            FileInfo fInfo = new FileInfo(filePath);
+            Console.WriteLine(fInfo);
+        }
+        public void FileCopyTo()
+        {
+            string destinationFilePath = @"H:\data.txt";
+            bool overwrite = true;
+            FileInfo fInfo = new FileInfo(filePath);
+            fInfo.CopyTo(destinationFilePath, overwrite);
+        }
+        public void FileDeleteTo()
+        {
+            string sourceFilePath = @"H:\data.txt";
+            FileInfo fInfo = new FileInfo(sourceFilePath);
+            fInfo.Delete();
+        }
+        public void DirectoryName()
+        {
+            string sourceFilePath = @"H:\data.txt";
+            FileInfo fInfo = new FileInfo(sourceFilePath);
+            string directoryPath = fInfo.DirectoryName;
+            Console.WriteLine(directoryPath);
+            //returns H:\
+        }
+        public void FileExist()
+        {
+            FileInfo fInfo = new FileInfo(filePath);
+            bool filesExists = fInfo.Exists;
+            Console.WriteLine(filesExists);
+        }
+        public void FileExtension()
+        {
+            FileInfo fInfo = new FileInfo(filePath);
+            string filesExtn = fInfo.Extension;
+            Console.WriteLine("File Extension is : " + filesExtn);
+        }
+        public void FileLength()
+        {
+            FileInfo fInfo = new FileInfo(filePath);
+            long length = fInfo.Length;
+            Console.WriteLine("file length is : " + length);
+        }
+        public void CreateDirectory()
+        {
+            string sourceDirPath = @"H:\MyDirectory\";
+            Directory.CreateDirectory(sourceDirPath);
+        }
+        public void DeleteDirectory()
+        {
+            string sourceDirPath = @"H:\MyDirectory\";
+            bool deleteRecursively = true;
+            Directory.Delete(sourceDirPath, deleteRecursively);
+        }
+        public void ExistDirectory()
+        {
+            string sourceDirPath = @"H:\Data\Data1";
+            bool tempDataDirectoryExists = Directory.Exists(sourceDirPath);
+            Console.WriteLine(tempDataDirectoryExists);
+        }
+        public void GetDirectory()
+        {
+            string sourceDirPath = @"H:\";
+            string[] subDirectories = Directory.GetDirectories(sourceDirPath);
+            foreach (string subDirectory in subDirectories)
+            {
+                Console.WriteLine(subDirectory);
+            }
+        }
+        public void GetFiles()
+        {
+            string sourceDirPath = @"C:\Users\";
+            string[] files = Directory.GetFiles(sourceDirPath);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        
+
+    }
+
+}
+
+
---
+++ OperationFile/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+namespace OperationFile
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Files f=new Files();
+            
+          //Program program = new Program();
+            Console.WriteLine("Select an option");
+            int option;
+            do
+            {
+                Console.WriteLine("1 for FileReadAllText");
+                Console.WriteLine("2 for FileReadAllLines");
+                Console.WriteLine("3 for FileReadAllBytes");
+                Console.WriteLine("4 for FileWriteAllText");
+                Console.WriteLine("5 for FileWriteAllLines");
+                Console.WriteLine("6 for FileAppendAllText");
+                Console.WriteLine("7 for FileAppendAllLines");
+                Console.WriteLine("8 for FileCopy");
+                Console.WriteLine("9 for FileDelete");
+                Console.WriteLine("10 for FileExistorNot");
+                Console.WriteLine("11 for GetFileCreationTime");
+                Console.WriteLine("12 for FileInfo");
+                Console.WriteLine("13 for FileCopyTo");
+                Console.WriteLine("14 for FileDeleteTo");
+                Console.WriteLine("15 for DirectoryName");
+                Console.WriteLine("16 for FileExist");
+                Console.WriteLine("17 for FileExtension");
+                Console.WriteLine("18 for FileLength");
+                Console.WriteLine("19 for CreateDirectory");
+                Console.WriteLine("20 for DeleteDirectory");
+                Console.WriteLine("21 for ExistDirectory");
+                Console.WriteLine("22 for GetDirectory");
+                Console.WriteLine("23 for GetFiles");
+                Console.WriteLine("0 for Exit");
+
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        // OperationFile.FileReadAllText();
+                        f.FileReadAllText();
+                        break;
+                    case 2:
+                        f.FileReadAllLines();
+                        break;
+                    case 3:
+                        f.FileReadAllBytes();
+                        break;
+                    case 4:
+                        f.FileWriteAllText();
+                        break;
+                    case 5:
+                        f.FileWriteAllLines();
+                        break;
+                    case 6:
+                        f.FileAppendAllText();
+                        break;
+                    case 7:
+                        f.FileAppendAllLines();
+                        break;
+                    case 8:
+                       f.FileCopy();
+                        break;
+                    case 9:
+                        f.FileDelete();
+                        break;
+                    case 10:
+                        f.FileExistorNot();
+                        break;
+                    case 11:
+                        f.GetFileCreationTime();
+                        break;
+                    case 12:
+                        f.FileInfo();
+                        break;
+                    case 13:
+                        f.FileCopyTo();
+                        break;
+                    case 14:
+                       f.FileDeleteTo();
+                        break;
+                    case 15:
+                        f.DirectoryName();
+                        break;
+                    case 16:
+                       f.FileExist();
+                        break;
+                    case 17:
+                       f.FileExtension();
+                        break;
+                    case 18:
+                        f.FileLength();
+                        break;
+                    case 19:
+                        f.CreateDirectory();
+                        break;
+                    case 20:
+                      f.DeleteDirectory();
+                        break;
+                    case 21:
+                        f.ExistDirectory();
+                        break;
+                    case 22:
+                   f.GetDirectory();
+                        break;
+                    case 23:
+                        f.GetFiles();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+            } while (option!= 0);
+        }
+    }
+}
